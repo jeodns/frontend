d50646c90f10b4e341964ac674b176c27e65f1b0
Behzad Khanlar <behkha@yahoo.com>
2023-01-10 17:01:07 +0330

    update favicon & jeo brand icon

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,20 +1,7 @@
 <template>
   <a class="d-flex flex-row" href="https://jeodns.com/" target="_blank">
     <p class="my-auto mx-2">JeoDNS</p>
-    <div
-      class="
-        polygon
-        my-auto
-        d-flex
-        flex-column
-        align-items-center
-        justify-content-center
-      "
-    >
-      <span class="regular-font">J</span>
-      <span class="regular-font">E</span>
-      <span class="regular-font">O</span>
-    </div>
+    <img src="@/assets/icons/jeo.svg"/>
   </a>
 </template>
 
@@ -27,7 +14,6 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-
 a {
   text-decoration: none;
 }
@@ -39,32 +25,5 @@ p {
   color: white;
   z-index: 3;
   opacity: 1;
-}
-
-.polygon {
-  width: 32px;
-  height: 32px;
-  -webkit-clip-path: polygon(
-    50% 0%,
-    100% 25%,
-    100% 75%,
-    50% 100%,
-    0% 75%,
-    0% 25%
-  );
-  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
-  background: linear-gradient(
-    180deg,
-    rgba(69, 149, 246, 1) 0%,
-    rgba(92, 199, 255, 1) 100%
-  );
-  z-index: 3;
-  opacity: 1;
-
-  & span {
-    color: white;
-    font-size: 8px;
-    margin: -2px;
-  }
 }
 </style>
